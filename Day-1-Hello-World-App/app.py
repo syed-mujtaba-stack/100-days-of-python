a83973eee5165abe28b71ec07260eae94c0c612f
M.J_syed <166711387+mujtabaabas@users.noreply.github.com>
2025-05-11 21:56:59 +0500

    Add files via upload

---
+++ Day-1-Hello-World-App/app.py
@@ -0,0 +1,10 @@
+# hello_world_app.py
+
+import streamlit as st
+
+# Title of the app
+st.title("🚀 Hello World Streamlit App")
+
+# Main text
+st.write("Welcome to your first Streamlit app!")
+st.write("👋 Hello, World!")
